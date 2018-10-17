6563dfcd8496a01376fd9135f8c42f6b3c6afa1c
isadoramesquita <isadoramesquitah@gmail.com>
2018-10-16 22:15:37 -0300

    Aula com PrimeFaces

---
+++ LoginPrimeFaces/src/login/UserLogin.java
@@ -0,0 +1,60 @@
+package login;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+import org.primefaces.PrimeFaces;
+
+@ManagedBean
+public class UserLogin {
+	 private String username;
+     
+	    private String password;
+	 
+	    public String getUsername() {
+	        return username;
+	    }
+	 
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+	 
+	    public String getPassword() {
+	        return password;
+	    }
+	 
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	   
+	    public void login(ActionEvent event) {
+	        FacesMessage message = null;
+	        boolean loggedIn = false;
+	         
+	        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
+	            loggedIn = true;
+	            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+	        } else {
+	            loggedIn = false;
+	            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+	        }
+	         
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+	        PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
+	    
+	        try {
+	        	if(loggedIn) {
+	        		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+	        		ec.redirect(ec.getRequestContextPath() + "/faces/galleria.xhtml");
+	        	}
+	        }catch (IOException e) {
+	        	e.printStackTrace();
+	        }
+	    
+	    }   
+}
